42df1935f04e21bea8a97ccd7f74a3ac59adf9c6
Andew Heiss <andrew@andrewheiss.com>
2010-01-06 15:58:24 +0200

    Added descriptionify filter - checks for excerpt in YAML front matter

--- _lib/filters.rb
+++ _lib/filters.rb
@@ -11,6 +11,16 @@ module Jekyll
     def last_of_year(post)
       post['next'].nil? or post['next'].date.year != post['date'].year
     end
+    
+    def descriptionify(excerpt)
+      if excerpt.nil?
+        description = 'Andrew Heiss is a student and web designer and developer from Utah who lives in Egypt, working on his MA in Middle East Studies at AUC.'
+      else
+        description = excerpt
+      end
+      
+      "#{description}"
+    end
 
   end
 end
